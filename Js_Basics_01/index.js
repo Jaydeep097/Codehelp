0443a4c6c103c5b3ccc953fe765bba121eecf832
Jaydeep Yaduwanshi <jaydeepyaduvanshi09@gmail.com>
2025-07-17 16:43:06 +0530

    Initial commit: JavaScript basics learning projects

---
+++ Js_Basics_01/index.js
@@ -0,0 +1,57 @@
+console.log("hello")
+
+
+
+// dynamic typing in javascript
+let lastName = "jaydeep";
+console.log(lastName);
+lastName = 3;
+console.log(lastName); 
+
+
+
+// ternary operator 
+
+let age = 15;
+let status =  (age>=18)? "can vote" : "cannot vote";
+console.log(status);
+
+
+// switch case example
+
+// switch(num){
+//          case 1 :
+//             console.log("A");
+//             break;
+//         case 2 :
+//             console.log("B");
+//             break;
+//         case 3 :
+//             console.log("C");
+//             break;
+//          default :
+//             console.log("D");
+//             break;
+        
+// }
+
+
+// loops 
+
+
+// for(let i = 0 ; i<=5; i++){
+//     console.log(i);
+// }
+
+// let i = 0;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+ 
+let i =0;
+do {
+    console.log(i);
+    i++;
+}while(i<=5);
+
---
+++ js_3/string/string.js
@@ -0,0 +1,62 @@
+// console.log('hey');
+
+
+// this is primitive type 
+
+// let lastName = 'Jaydeep';
+ 
+
+// and this is the  object  -= we use string constructor function to make string to object 
+
+// let firstName = new String('love');
+
+
+// template litral same lokks as it is like return in code 
+// let message = `hello  ${lastName}
+
+//  hope you are well
+ 
+//  regards love babbar`;
+
+//  console.log(message);
+
+
+
+
+
+
+
+
+
+
+// console.log('Hello Jee');
+
+// let lastName = 'Babbar' ;
+
+// let firstName = new String('Love');
+
+// let message = 
+// `Hello ${lastName},
+
+// Thanks for the Opportunity
+
+// Regards,
+// Babbar`;
+
+// console.log(message);
+
+// let words = message.split(' ');
+
+// console.log(words);
+
+// let date = new Date();
+
+
+// let date2 = new Date('June 20 1998 07:15');
+
+// let date3 = new Date(1998, 11, 20, 7);
+
+
+// date3.setFullYear(1947);
+
+// console.log(date3);
