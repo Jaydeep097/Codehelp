90780adf59581de57b1294122d74bbacbe8f9486
Jaydeep Yaduwanshi <jaydeepyaduvanshi09@gmail.com>
2025-07-20 21:41:45 +0530

    Your commit message here

---
+++ Js_Basics_04/index.js
@@ -0,0 +1,38 @@
+// // console.log('kya haaal');
+
+
+// //function call or invoke
+// run();
+
+
+// //function declaration
+// function run() {
+//     console.log('running');
+// }
+
+
+
+
+// //Named function assignment
+// let stand = function walk() {
+//     console.log('walking');
+// };
+
+// //Anonymous function assignment
+// let stand2 = function() {
+//     console.log('walking');
+// };
+
+// stand();
+
+// let jump = stand;
+
+// jump();
+
+// stand2();
+
+
+// let x = 1;
+// x = 'a';
+
+// console.log(x);
