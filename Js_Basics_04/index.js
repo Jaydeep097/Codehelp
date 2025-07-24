b1c38760dec4dc6856010e2b932153eed83bec1a
Jaydeep097 <jaydeepyaduvanshi09@gmail.com>
2025-07-24 18:40:38 +0530

    getter and setter with try and cathch

--- Js_Basics_04/index.js
+++ Js_Basics_04/index.js
@@ -53,6 +53,8 @@
 // let ans = sum(1,2,2,3);
 // console.log(ans);
 
+
+ 
 // Rest operator
 
 // function sum(num, value , ...args){
@@ -63,9 +65,91 @@
 
 
 
+
 // Default parameters
 
-function interest(p,r,y){
-    return p*r*y/100 ;
+// function interest(p,r=2,y=4){
+//     return p*r*y/100 ;
+// }
+// console.log(interest(1000,33));
+// if we want to bypass default parameter
+// console.log(interest(1000,undefined,6));
+
+
+
+// GETTER SETTER
+// getter -> access properties
+// setter -> change or mutate  properties
+
+
+// let person = {
+//     fName : 'jaydeep',
+//     lName : 'yaduwanshi',
+
+//     get fullName(){
+//          return `${person.fName}    ${person.lName} ` },
+
+//     set fullName(value){
+//         let parts = value.split(' ');
+//         this.fName = parts[0];   // jis bhi object par kam kar rha uska first name
+//         this.lName = parts[1];
+        
+//     }
+// };
+ 
+// console.log(person.fullName);
+// person.fullName = "rahul rajput";
+
+
+// // console.log(person.fullName);
+
+  
+// try {
+//     person.fullName = true;
+// }
+// catch (e) {
+//     alert(e);
+// }
+
+// console.log(person.fullName);
+
+
+// GETTER SETTER
+
+// getter -> access properties
+// setter -> change or mutate properties
+
+
+let person = {
+    fName : 'Love',
+    lName : 'Babbar',
+    get fullName() {
+        return `${person.fName} ${person.lName}`;
+    },
+    set fullName(value) {
+        if(typeof value !== String) {
+            throw new Error("You have not sent a string");
+        }
+        let parts = value.split(' ');
+        this.fName = parts[0];
+        this.lName = parts[1];
+    }
+};
+
+
+
+console.log(person);
+// issue - > read only
+
+
+console.log(person.fullName);
+
+try {
+    person.fullName = true;
 }
-console.log(interest(1000,33,6));
+catch (e) {
+    alert(e);
+}
+
+
+console.log(person.fullName);
