97a2cc03329d64be95bb36e0e21b0dd567799240
Jaydeep097 <jaydeepyaduvanshi09@gmail.com>
2025-07-27 17:14:13 +0530

    Add DOM event handling and dynamic element creation - DOM_Modern_js_02

--- JavaScript/DOM_Modern_js_02/index.js
+++ JavaScript/DOM_Modern_js_02/index.js
@@ -12,13 +12,40 @@
 //     console.log(babbar);
 // })
 
-let links = document.querySelectorAll('a');
 
-let thirdLink =  links[2];
 
-thirdLink.addEventListener('click', function(event){
-    event.preventDefault();
-    console.log('thik hai acha laga');
 
-});
+// this -> prevent default functionality of any function 
 
+// select all anchor tags
+// let links = document.querySelectorAll('a');
+
+// let thirdLink =  links[2];   // index 2
+
+// thirdLink.addEventListener('click', function(event){
+//     event.preventDefault();
+//     console.log('thik hai acha laga');
+
+// });
+
+
+
+let myDiv =  document.createElement('div');
+
+function paraStatus(event){
+        console.log('clicked on para ' + event.target.textContent );
+    }
+  
+   myDiv.addEventListener('click', paraStatus);
+
+for (let i=1; i <=100; i++ ){
+    let newElement = document.createElement('p');
+    newElement.textContent = 'THIS IS PARA ' + i;
+
+
+    myDiv.appendChild(newElement);
+}
+document.body.appendChild(myDiv);
+
+
+ 
